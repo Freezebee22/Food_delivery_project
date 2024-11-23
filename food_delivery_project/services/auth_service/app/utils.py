5bfc8ec5eb67618c5188897f139628e150fee512
freezebee <stanislavlav05@gmail.com>
2024-11-23 15:42:47 +0300

    Update utils.py

--- food_delivery_project/services/auth_service/app/utils.py
+++ food_delivery_project/services/auth_service/app/utils.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from passlib.context import CryptContext
 import jwt
 from datetime import datetime, timedelta
@@ -22,3 +23,12 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
